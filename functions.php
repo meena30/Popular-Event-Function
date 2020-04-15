415012df95153485f6a00159ae3752ffebf7b220
meena30 <40920232+meena30@users.noreply.github.com>
2020-04-15 16:40:45 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,15 @@
+/*
+ * Set event views count using post meta
+ */
+function setEventViews($postID) {
+    $countKey = 'event_views_count';
+    $count = get_post_meta($postID, $countKey, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $countKey);
+        add_post_meta($postID, $countKey, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $countKey, $count);
+    }
+}
